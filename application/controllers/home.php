20ec1c0581c839b2cfd19f6e5e0adf2e6ec70390
yhdeng <yhdeng@server.com>
2015-02-27 13:27:34 +0800

    后台左侧固定

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -10,5 +10,7 @@ class Home extends CI_Controller {
 	{
 		$this->smarty->assign("title", "Home");
 		$this->smarty->display("home.html");
+		// $this->smarty->fetch("home.html"); //获取输出内容，但是输出到浏览器
+		// print_r($string);
 	}
 }
